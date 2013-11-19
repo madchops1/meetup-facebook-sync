1bdc3122ecf1e8c47430706519e4af8d0abff75b
Karl Steltenpohl <karl@webksd.com>
2013-11-19 17:04:56 -0600

    -kjs

--- mfbsyc.php
+++ mfbsyc.php
@@ -11,9 +11,13 @@ error_reporting(E_ALL);
 
 class mfbsync {
   
-  var $meetup_redirect_uri       = "http://mfbsync.karlsteltenpohl.com/meetup-redirect.php";
+  var $meetup_redirect_uri       = "http://mfbsync.karlsteltenpohl.com";
   var $meetup_consumer_key       = "drdhpm0c4l1haeem1evkcdk2h5";
   var $meetup_consumer_secret    = "r0jqlpdk6hdqsspb17iq7iftt"; 
+  
+  var $facebook_redirect_uri     = "http://mfbsync.karlsteltenpohl.com";
+  var $facebook_consumer_key     = "588715921194851";
+  var $facebook_consumer_secret  = "5509ae4d80411edb07787535d55e144f";
   
   /**
    * Constructor // Init...
@@ -24,6 +28,7 @@ class mfbsync {
     if (!isset($_SESSION['meetup_auth'])) { $_SESSION['meetup_auth'] = FALSE; }
     
     $this->meetup_auth();
+    $this->facebook_auth();
     $this->finish_up();
     
   }  
@@ -33,9 +38,8 @@ class mfbsync {
    */
   function meetup_auth(){
     
-  
     // -- Set Response from Step 1, save code
-    if($_REQUEST['code'] != ''){
+    if($_REQUEST['code'] != '' && $_SESSION['meetup_auth'] == FALSE){
       $_SESSION['meetup_auth'] = TRUE;
       $_SESSION['meetup_auth_code'] = $_REQUEST['code'];
     }
@@ -48,36 +52,63 @@ class mfbsync {
     
     // -- Step 2, Get access token from meetup
     $ch = curl_init();
-    //Set the URL to work with
     curl_setopt($ch, CURLOPT_URL, 'https://secure.meetup.com/oauth2/access');
     curl_setopt($ch, CURLOPT_POSTFIELDS, 'client_id='.$this->meetup_consumer_key.
                                          '&client_secret='.$this->meetup_consumer_secret.
                                          '&grant_type=authorization_code'.
                                          '&redirect_uri='.$this->meetup_redirect_uri.
                                          '&code='.$_SESSION['meetup_auth_code'].'');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $return = curl_exec($ch);
+    curl_close($ch);
     
-    // ENABLE HTTP POST
-    //curl_setopt($ch, CURLOPT_POST, 1);
-    // FOllOW REDIRECTS
-    //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    // Set the post parameters
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, 'client_id='.$this->instagramClientId.'&client_secret='.$this->instagramClientSecret.'&grant_type=authorization_code&redirect_uri='.$redirectURI.'&code='.$code.'');
-    //Setting CURLOPT_RETURNTRANSFER variable to 1 will force cURL
-    //not to print out the results of its query.
-    //Instead, it will return the results as a string return value
-    //from curl_exec() instead of the usual true/false.
+    $meetup_response = json_decode($return);
+    // -- Successfull response...
+    if(isset($meetup_reponse->token_type) && $meetup_response->token_type == "bearer"){
+      $_SESSION['meetup_refresh_token'] = $meetup_response->refresh_token;
+      $_SESSION['meetup_access_token'] = $meetup_response->access_token;
+    }
+  }
+  
+  function facebook_auth(){
+    
+    // -- Set Response from Step 1, save code
+    if($_REQUEST['code'] && $_SESSION['facebook_auth'] == FALSE){
+      $_SESSION['facebook_auth'] = TRUE;
+      $_SESSION['facebook_auth_code'] = $_REQUEST['code'];
+    }
+    
+    // -- Step 1, Direct User to Facebook for Authentication
+    if($_SESSION['facebook_auth'] == FALSE){
+      header("LOCATION: https://www.facebook.com/dialog/oauth?client_id='.$this->facebook_consumer_key.'&redirect_uri='.$this->facebook_redirect_uri.'");
+    }
+    
+    // -- Step 2, Get access token from user
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/oauth/access_token?'.
+                                  'client_id='.$this->facebook_consumer_key.
+                                  '&redirect_uri='.$this->facebook_redirect_uri.
+                                  '&client_secret='.$this->facebook_consumer_secret.
+                                  '&code='.$_SESSION['facebook_auth_code']);
+                               
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     //execute the request
     $return = curl_exec($ch);
     curl_close($ch);
-    
     var_dump($return);
+    die;
+    
+    // -- Step 3, Get access token from page
+    
+    
   }
   
   /**
    * Finish up
    */
   function finish_up(){
+    var_dump($_REQUEST);
     echo "Done!";
   }
   
