3b2caca7db99a0336d87c8229f30b856ac5dd33f
Karl Steltenpohl <karl@webksd.com>
2013-11-19 18:09:57 -0600

    -kjs

--- mfbsyc.php
+++ mfbsyc.php
@@ -46,36 +46,37 @@ class mfbsync {
    * Meetup Auth Function
    */
   function meetup_auth(){
-    
-    // -- Set Response from Step 1, save code
-    if($_REQUEST['code'] != '' && $_SESSION['meetup_auth'] == FALSE){
-      $_SESSION['meetup_auth'] = TRUE;
-      $_SESSION['meetup_auth_code'] = $_REQUEST['code'];
-    }
-    
-    // -- Step 1, Direct User to Meetup for authentication
-    if($_SESSION['meetup_auth'] == FALSE) {
-      header("LOCATION: https://secure.meetup.com/oauth2/authorize?client_id=".$this->meetup_consumer_key."&response_type=code&redirect_uri=".$this->meetup_redirect_uri."");
-      exit;
-    }
-    
-    // -- Step 2, Get access token from meetup
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://secure.meetup.com/oauth2/access');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, 'client_id='.$this->meetup_consumer_key.
-                                         '&client_secret='.$this->meetup_consumer_secret.
-                                         '&grant_type=authorization_code'.
-                                         '&redirect_uri='.$this->meetup_redirect_uri.
-                                         '&code='.$_SESSION['meetup_auth_code'].'');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $return = curl_exec($ch);
-    curl_close($ch);
-    
-    $meetup_response = json_decode($return);
-    // -- Successfull response...
-    if(isset($meetup_reponse->token_type) && $meetup_response->token_type == "bearer"){
-      $_SESSION['meetup_refresh_token'] = $meetup_response->refresh_token;
-      $_SESSION['meetup_access_token'] = $meetup_response->access_token;
+    if(!isset($_SESSION['meetup_access_token'])){
+      // -- Set Response from Step 1, save code
+      if($_REQUEST['code'] != '' && $_SESSION['meetup_auth'] == FALSE){
+        $_SESSION['meetup_auth'] = TRUE;
+        $_SESSION['meetup_auth_code'] = $_REQUEST['code'];
+      }
+      
+      // -- Step 1, Direct User to Meetup for authentication
+      if($_SESSION['meetup_auth'] == FALSE) {
+        header("LOCATION: https://secure.meetup.com/oauth2/authorize?client_id=".$this->meetup_consumer_key."&response_type=code&redirect_uri=".$this->meetup_redirect_uri."");
+        exit;
+      }
+      
+      // -- Step 2, Get access token from meetup
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, 'https://secure.meetup.com/oauth2/access');
+      curl_setopt($ch, CURLOPT_POSTFIELDS, 'client_id='.$this->meetup_consumer_key.
+                                           '&client_secret='.$this->meetup_consumer_secret.
+                                           '&grant_type=authorization_code'.
+                                           '&redirect_uri='.$this->meetup_redirect_uri.
+                                           '&code='.$_SESSION['meetup_auth_code'].'');
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $return = curl_exec($ch);
+      curl_close($ch);
+      
+      $meetup_response = json_decode($return);
+      // -- Successfull response...
+      if(isset($meetup_reponse->token_type) && $meetup_response->token_type == "bearer"){
+        $_SESSION['meetup_refresh_token'] = $meetup_response->refresh_token;
+        $_SESSION['meetup_access_token'] = $meetup_response->access_token;
+      }
     }
   }
   
@@ -84,60 +85,48 @@ class mfbsync {
    */
   function facebook_auth(){
     
-    // -- Set Response from Step 1, save code
-    if($_REQUEST['code'] && $_SESSION['facebook_auth'] == FALSE){
-      $_SESSION['facebook_auth'] = TRUE;
-      $_SESSION['facebook_auth_code'] = $_REQUEST['code'];
+    if(!isset($_SESSION['facebook_access_token'])){
+      // -- Set Response from Step 1, save code
+      if($_REQUEST['code'] && $_SESSION['facebook_auth'] == FALSE){
+        $_SESSION['facebook_auth'] = TRUE;
+        $_SESSION['facebook_auth_code'] = $_REQUEST['code'];
+      }
+      
+      // -- Step 1, Direct User to Facebook for Authentication
+      if($_SESSION['facebook_auth'] == FALSE){
+        header("LOCATION: https://www.facebook.com/dialog/oauth?client_id=".$this->facebook_consumer_key."&scope=manage_pages&redirect_uri=".urlencode($this->facebook_redirect_uri));
+      }
+      
+      // -- Step 2, Get access token from user
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/oauth/access_token?'.
+                                    'client_id='.$this->facebook_consumer_key.
+                                    '&redirect_uri='.urlencode($this->facebook_redirect_uri).
+                                    '&client_secret='.$this->facebook_consumer_secret.
+                                    '&code='.$_SESSION['facebook_auth_code']);
+                                 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      //execute the request
+      $return = curl_exec($ch);
+      curl_close($ch);
+      
+      if(strstr($return, "access_token")){
+        $returnArray = explode("&",$return);
+        $tokenArray = explode("=",$returnArray[0]);
+        $_SESSION['facebook_access_token'] = $tokenArray[1];
+        //$_SESSION['facebook_expires'] = $tokenArray
+      } else {
+        $facebook_response = json_decode($return);
+      }
+      
+      // If Error
+      if(isset($facebook_response->error)){
+        $_SESSION['facebook_auth'] = FALSE;
+        $_SESSION['facebook_auth_code'] = "";
+        unset($_SESSION['facebook_access_token']);
+        die("Error Please Try Again..."); // -kjs sometimes we get an error here...
+      }
     }
-    
-    // -- Step 1, Direct User to Facebook for Authentication
-    if($_SESSION['facebook_auth'] == FALSE){
-      header("LOCATION: https://www.facebook.com/dialog/oauth?client_id=".$this->facebook_consumer_key."&scope=manage_pages&redirect_uri=".urlencode($this->facebook_redirect_uri));
-    }
-    
-    // -- Step 2, Get access token from user
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://graph.facebook.com/oauth/access_token?'.
-                                  'client_id='.$this->facebook_consumer_key.
-                                  '&redirect_uri='.urlencode($this->facebook_redirect_uri).
-                                  '&client_secret='.$this->facebook_consumer_secret.
-                                  '&code='.$_SESSION['facebook_auth_code']);
-                               
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //execute the request
-    $return = curl_exec($ch);
-    curl_close($ch);
-    
-    if(strstr($return, "access_token")){
-      $returnArray = explode("&",$return);
-      $tokenArray = explode("=",$returnArray[0]);
-      $_SESSION['facebook_access_token'] = $tokenArray[1];
-      //$_SESSION['facebook_expires'] = $tokenArray
-    } else {
-      $facebook_response = json_decode($return);
-    }
-    
-    // If Error
-    if(isset($facebook_response->error)){
-      $_SESSION['facebook_auth'] = FALSE;
-      $_SESSION['facebook_auth_code'] = "";
-      die("Error Please Try Again..."); // -kjs sometimes we get an error here...
-    }
-    
-    // 
-    
-    
-    //echo "FB RES:<pre>";
-    //var_dump($facebook_response);
-    //echo "</pre>";
-    //die();
-    // If code is expired then get a new code
-    //if($)
-    //die;
-    
-    // -- Step 3, Get access token from page
-    
-    
   }
   
   /**
