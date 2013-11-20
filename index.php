3d86396fdd35e4e33f73b184255b623da3337368
Karl Steltenpohl <karl@webksd.com>
2013-11-20 17:08:03 -0600

    -kjs

--- index.php
+++ index.php
@@ -31,19 +31,20 @@ if($_REQUEST['meetup_name'] && $_REQUEST['fb_page_id']){
   
   </head>
   <body>
-    <h1>Meetup.com <=> Facebook Event Sync</h1>
-    <div style='margin:0px auto; width:500px; margin-top:200px; background:#f5f5f5; padding:20px; font-size:10px;'>
+    <div style='margin:0px auto; width:500px; margin-top:200px; background:#f5f5f5; padding:20px; font-size:12px;'>
+      <h1>Meetup.com <=> Facebook Event Sync</h1>
+    
       <form>
         
         <div>
           <label>Meetup Name</label>
           <input type='text' name='meetup_name' value='' /> ex. "Chicago-Foosball"
-        </div>
+        </div><br>
         
         <div>
           <label>FB Page ID</label>
           <input type='text' name='fb_page_id' value='' /> ex. "474860665902713"
-        </div>
+        </div><br>
         
         <div>
         <input type='submit' value='Sync' />
