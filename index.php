0053aa5cb237aa353f706b50127d659e6e641d99
Karl Steltenpohl <karl@webksd.com>
2013-11-20 23:53:49 -0600

    -kjs

--- index.php
+++ index.php
@@ -124,6 +124,13 @@ if($_REQUEST['meetup_name'] && $_REQUEST['fb_page_id']){
         font-size:10px;
         width:500px;
         margin:10px auto;
+        color:#fff;
+      }
+      
+      .big-footer{
+        background-color:#333;
+        width:100%;
+        padding:20px 0px;
       }
       
       
@@ -496,12 +503,13 @@ if($_REQUEST['meetup_name'] && $_REQUEST['fb_page_id']){
       
     </div>
     
-    <div class='footer'>
-     &copy; <?php echo date("Y"); ?> Karl Steltenpohl Development LLC. All Rights Reserved. <br>
-     In No Way Affiliated with Facebook, Meetup.com, or Google.<br>
-     <a href='mailto:syncsupport+karl@webksd.com;'>Get Help!</a>
+    <div class='big-footer'>
+      <div class='footer'>
+       &copy; <?php echo date("Y"); ?> Karl Steltenpohl Development LLC. All Rights Reserved. <br>
+       In No Way Affiliated with Facebook, Meetup.com, or Google.<br>
+       <a href='mailto:syncsupport+karl@webksd.com;'>Get Help!</a>
+      </div>
     </div>
-
     
   </body>
 </html>
