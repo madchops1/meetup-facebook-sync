55ab78fe0ea84096b6f69913147f067c1adcd759
Karl Steltenpohl <karl@webksd.com>
2013-11-21 19:58:19 -0600

    -kjs

--- cron.php
+++ cron.php
@@ -15,6 +15,7 @@ $ii = 0;
 while($row = mysql_fetch_object($result)){
   $ii++;
   echo "Rel ".$ii."<br>";
+  continue;
   // -- Load The User Object
   $select = "  SELECT * FROM users WHERE id='".$row->uid."' LIMIT 1";
   $user_object = mysql_fetch_object(mysql_query($select));
